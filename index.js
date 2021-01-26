97227e50955679902c0e2a18ff15624c11eddf36
Vaincentel <P.Burula228@mail.ru>
2021-01-26 09:18:16 +0300

    птчапти
    имт

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send('please ddos grisha!')
 })
+
+app.get('/grisha', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send('DDOS GRISHA!')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+})ы
